cf1f3674c0756e058dbceebf8796c8a1a9c5a13b
jhonnywinarta <jhonnywinarta@gmail.com>
2021-05-24 11:20:15 +0800

    2021上機考v3_(黃裕洳)

--- app/src/main/java/tw/edu/pu/o1083023/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083023/crazyshape/MainActivity.kt
@@ -3,29 +3,49 @@ package tw.edu.pu.o1083023.crazyshape
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Author:黃裕洳", Toast.LENGTH_LONG).show()
+class MainActivity : AppCompatActivity(){
 
-        Glide.with(this)
-            .load(R.drawable.cover)
-            .override(800, 600)
-            .into(imageTitle)
+    fun ChangeShape() {
+        val X: Int = (0..3).random()
 
-        imgNext.setOnLongClickListener(object: View.OnLongClickListener{
-            override fun onLongClick(v: View?): Boolean {
-                intent = Intent(this@MainActivity, GameActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-        })
+        when (X) {
+            0 -> imgNext.setImageResource(R.drawable.circle)
+            1 -> imgNext.setImageResource(R.drawable.square)
+            2 -> imgNext.setImageResource(R.drawable.star)
+            3 -> imgNext.setImageResource(R.drawable.triangle)
+        }
     }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            Toast.makeText(this, "Author:黃裕洳", Toast.LENGTH_LONG).show()
+
+            Glide.with(this)
+                .load(R.drawable.cover)
+                .override(800, 600)
+                .into(imageTitle)
+
+            imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+                override fun onLongClick(v: View?): Boolean {
+                    intent = Intent(this@MainActivity, GameActivity::class.java)
+                    startActivity(intent)
+                    return true
+                }
+            })
+
+            imgNext.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    ChangeShape()
+                }
+            })
+        }
 }
